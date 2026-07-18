c3fdc3a3998019b283d827e1b1c4d30fccc3a8be
Ishika-om19 <somi37987@gmail.com>
2026-07-19 00:07:59 +0530

    cloud setup

--- routes/listing.js
+++ routes/listing.js
@@ -5,6 +5,8 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
 
 //Index & Create route using router.route
 router
