df58d86a2d2cf946291d5f4dc165bee06a5aecb5
Ishika-om19 <somi37987@gmail.com>
2026-09-24 19:23:52 +0530

    Deployment setup

--- app.js
+++ app.js
@@ -111,6 +111,11 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("listings/error.ejs",{message});
 });
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`);
 });
+
+// app.listen(8080,()=>{
+//     console.log("server is listening to port 8080");
+// });
